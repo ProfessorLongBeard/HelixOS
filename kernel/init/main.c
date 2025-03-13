ed620ce97d653e8e68a86c062b3327fd35ac68c4
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-13 13:17:29 -0700

    Init flanterm with Helix

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -1,18 +1,11 @@
+#include <kstdio.h>
 #include <kstring.h>
 #include <kernel.h>
-#include <limine.h>
 #include <arch.h>
+#include <framebuffer.h>
 #include <stdbool.h>
-#include <devices/pl011.h>
 
 
-
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0
-};
 
 
 
@@ -22,13 +15,11 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
 
 
 void helix_init(void) {
-    struct flanterm_context *ft = NULL;
-
     if (LIMINE_BASE_REVISION_SUPPORTED == false) {
         __hcf();
     }
 
-    struct limine_framebuffer *fb = framebuffer_request.response->framebuffers[0];
+    fb_init();
 
     __hcf();
 }
