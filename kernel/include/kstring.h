2282063132bf8f36505f40f699b3bdcd4a369b10
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-13 02:14:05 -0700

    Implement strlen + temporary truncfdf2()

--- kernel/include/kstring.h
+++ kernel/include/kstring.h
@@ -11,5 +11,8 @@
 
 void memset(void *dst, char ch, size_t len);
 void memcpy(void *dst, void *src, size_t len);
+size_t strlen(const char *s);
+
+double __trunctfdf2(long double a);
 
 #endif
---
+++ kernel/lib/kstring/strlen.c
@@ -0,0 +1,17 @@
+#include <kstring.h>
+#include <stddef.h>
+
+
+
+
+
+size_t strlen(const char *s) {
+    size_t len = 0;
+
+    while(*s != '\0') {
+        len++;
+        s++;
+    }
+
+    return len;
+}
