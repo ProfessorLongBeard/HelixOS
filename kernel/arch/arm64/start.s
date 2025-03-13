2de50b3c5351dfb01ef336f64d6431b39b9565af
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-12 23:22:38 -0700

    Check core number on startup

--- kernel/arch/arm64/start.s
+++ kernel/arch/arm64/start.s
@@ -5,8 +5,17 @@
 
 
 _start:
-    b helix_init
+    // Ensure we're running as core #0
+    mrs x15, mpidr_el1
+    and x15, x15, #0x3
+    cmp x15, #0
+    bne halt
 
+    // Initialize vbar_el1
+    adrp x15, _vector_table
+    msr vbar_el1, x15
+
+    b helix_init
 
 halt:
     wfi
