22d019cf0faf56fa4e64ed8aca49e95cd4e8305e
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-15 22:23:12 -0700

    PMM: Rework freelist initialization

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -10,7 +10,8 @@ static volatile struct limine_memmap_request mm = {
 };
 
 
-static freelist_t *fl;
+static freelist_t *head;
+
 
 
 
@@ -28,95 +29,48 @@ void pmm_init(void) {
     for (uint32_t i = 0; i < m->entry_count; i++) {
         struct limine_memmap_entry *e = m->entries[i];
 
-        if (e->type != LIMINE_MEMMAP_USABLE && e->type != LIMINE_MEMMAP_BOOTLOADER_RECLAIMABLE) {
+        // We only want usable regions
+        if (e->type != LIMINE_MEMMAP_USABLE) {
             continue;
         }
 
-        // Get first free usable region
-        if (!fl && e->type == LIMINE_MEMMAP_USABLE) {
-            // NOTE: In kerenl space we must use the HHDM offset to access physical memory
-            fl = (freelist_t *)(hhdm_offset + e->base);
-            fl->next = NULL;
+        if (!head) {
+            // Set first node
+            freelist_t *first_node = (freelist_t *)(hhdm_offset + e->base);
 
-            printf("PMM: Freelist initial base: [0x%lx - 0x%lx], size = %u\n", e->base, e->base + sizeof(freelist_t), sizeof(freelist_t));
+            first_node->base_addr = e->base;
+            first_node->length = e->length;
 
-            freelist_t *first_node = (freelist_t *)(hhdm_offset + e->base + sizeof(freelist_t));
-            first_node->base_addr = e->base + sizeof(freelist_t);
-            first_node->length = e->length - sizeof(freelist_t);
+            // Adjust base and size to account for node pointer
+            first_node->base_addr += sizeof(freelist_t);
+            first_node->length -= sizeof(freelist_t);
             first_node->next = NULL;
 
-            // Set first node
-            fl->next = first_node;
+            printf("PMM: Head node: [0x%lx - 0x%lx] size = %uKB\n", first_node->base_addr, first_node->base_addr + first_node->length, first_node->length / 1024);
+            head = first_node;
+        } else {
 
+            // Keep list head intact
+            freelist_t *prev = head;
+
+            // Iterate to last node
+            while(prev->next != NULL) {
+                prev = prev->next;
+            }
+
+            // Create new node and link it
+            freelist_t *node = (freelist_t *)(hhdm_offset + e->base);
+
+            node->base_addr = e->base;
+            node->length = e->length;
+
+            // Adjust base and size to account for freelist pointer
+            node->base_addr += sizeof(freelist_t);
+            node->length -= sizeof(freelist_t);
+
+            // Link node
+            prev->next = node;
+            printf("PMM: Node region: [0x%lx - 0x%lx] size = %uKB\n", node->base_addr, node->base_addr + node->length, node->length / 1024);
         }
-        // Iterate memory map and fill in next nodes
-        freelist_t *node = (freelist_t *)(hhdm_offset + e->base);
-        node->base_addr = e->base;
-        node->length = e->length;
-        node->next = NULL;
-
-        printf("PMM: New node region: [0x%lx - 0x%lx], size = %uKB\n", node->base_addr, node->base_addr + node->length, node->length / 1024);
-
-        node->next = fl->next;
-        fl = node;
     }
-}
-
-static void *__pmm_alloc_chunk(size_t len) {
-    freelist_t *curr = fl;
-    void *ptr = NULL;
-    uint64_t hhdm_offset = vmm_get_hhdm_offset();
-
-
-    ptr = (void *)(hhdm_offset + curr->base_addr);
-
-    curr->base_addr += len;
-    curr->length -= len;
-
-    printf("PMM: Allocated chunk: base = 0x%lx, end = 0x%lx, size = %u\n", curr->base_addr, curr->base_addr + len, len);
-    return ptr;
-}
-
-static void __pmm_free_chunk(void *ptr, size_t len) {
-}
-
-void *pmm_alloc(size_t len) {
-    freelist_t *curr_node = fl;
-    freelist_t *prev = NULL;
-    uint64_t hhdm_offset = vmm_get_hhdm_offset();
-
-
-
-
-    if (len < curr_node->length) {
-        void *ptr = __pmm_alloc_chunk(len);
-
-        if (!ptr) {
-            printf("PMM: Failed to allocate chunk size: %u!\n", len);
-            return NULL;
-        }
-
-        return ptr;
-    }
-
-    if (len > curr_node->length) {
-        // Iterate nodes till we can satisfy the allocation request
-        while(curr_node != NULL && len > curr_node->length) {
-            prev = curr_node;
-            curr_node = curr_node->next;
-        }
-
-        // Found new node to satisfy the allocation request
-        void *ptr = __pmm_alloc_chunk(len);
-
-        if (!ptr) {
-            printf("PMM: Failed to allocate chunk size: %u\n", len);
-            return NULL;
-        }
-
-        return ptr;
-    }
-
-    printf("PMM: No available memory for allocation!\n");
-    return NULL;
 }
