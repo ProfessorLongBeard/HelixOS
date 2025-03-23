9283ff3dabf32ea85945af1b7ac0af1fe93fc7cc
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-22 23:42:15 -0700

    Export kernel symbols

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -1,11 +1,30 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
 #include <limine.h>
 
 
 
-__attribute__((used, section(".limine_requests")))
-static volatile LIMINE_BASE_REVISION(3);
+extern uint64_t __kernel_start[];
+extern uint64_t __kernel_end[];
+
+extern uint64_t __stext[];
+extern uint64_t __etext[];
+
+extern uint64_t __srodata[];
+extern uint64_t __erodata[];
+
+extern uint64_t __sdata[];
+extern uint64_t __edata[];
+
+extern uint64_t __sbss[];
+extern uint64_t __ebss[];
+
+extern uint64_t __slimine_requests[];
+extern uint64_t __elimine_requests[];
+
+extern uint64_t __sstack[];
+extern uint64_t __estack[];
 
 #endif
