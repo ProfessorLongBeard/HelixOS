4906b5f82055f58dbad95620258eae4a86a6fc55
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-27 04:10:44 -0700

    GICv3: Various fixes

--- kernel/devices/gicv3.c
+++ kernel/devices/gicv3.c
@@ -7,7 +7,7 @@
 
 /*
  * Software Generated Interrupt INTID's: 0 - 15
- * Private Peripheral Interrupt INTID"s: 16 - 31
+ * Private Peripheral Interrupt INTID's: 16 - 31
  * Shared Peripheral Interrupt INTID's:  32 - 1029
  * Special Interrupt Number(s) INTID's:  1020 - 1023
  * Reserved INTID's:                     1024 - 8191
@@ -32,31 +32,40 @@
 
 
 void gic_init(void) {
-    GICD_CTLR = GICD_CTLR_ENABLE_GRP0 | GICD_CTLR_ENABLE_GRP1_NS | GICD_CTLR_ENABLE_GRP1_S | GICD_CTLR_ENABLE_ARE_S | GICD_CTLR_ENABLE_ARE_NS;
+    GICD_CTLR = GICD_CTLR_ENABLE_GRP1_NS | GICD_CTLR_ENABLE_ARE_NS;
+
+    // Wait for register writes in distributor to take effect. This will prevent
+    // any race conditions that might cause an incorrectly configured GIC
+    while(GICD_CTLR & GICD_CTLR_ENABLE_RWP);
 
     // Wake GIC CPU interface
-    uint32_t waker = GICR_WAKER;
+    uint32_t waker = GICR_WAKER(0);
     waker &= ~(GICR_WAKER_CPU_SLEEP);
-    GICR_WAKER = waker;
+    GICR_WAKER(0) = waker;
 
-    while(GICR_WAKER & GICR_WAKER_CPU_SLEEP);
+    // Wait for children to wake up
+    while(GICR_WAKER(0) & GICR_WAKER_CHILDREN_SLEEP);
 
     uint64_t sre = __icc_sre_read();
-    sre |= 1;
+    sre |= ICC_SRE_ENABLE;
     __icc_sre_write(sre);
 
-    uint32_t ctlr = __icc_ctlr_read();
-    ctlr |= ICC_CTLR_EOI_MODE;
-    __icc_ctlr_write(ctlr);
-
-    // Set default priority
-    __icc_pmr_write(0xA0);
+    // Allow all interrupts
+    __icc_pmr_write(0xFF);
 
     __icc_brp1_write(0b111);
 
+    uint64_t grp0 = __icc_igrpen0_read();
+    grp0 &= ~(ICC_IGRPEN0_ENABLE);
+    __icc_igrpen0_write(grp0);
+
     uint64_t grp1 = __icc_igrpen1_read();
-    grp1 |= 1;
+    grp1 |= ICC_IGRPEN1_ENABLE;
     __icc_igrpen1_write(grp1);
+
+    uint32_t ctlr = __icc_ctlr_read();
+    ctlr |= ICC_CTLR_CBPR | ICC_CTLR_EOI_MODE;
+    __icc_ctlr_write(ctlr);
 
     __daif_clr();
     printf("GIC: Initialized!\n");
@@ -76,41 +85,89 @@ void gic_enable_irq(int irq) {
     int reg = irq / 32;
     int bit = irq % 32;
 
-    GICD_ISENABLER(reg) = (1UL << bit);
+
+    if (irq < 32) {
+        GICR_ISENABLER0(0) |= (1UL << irq);
+    } else {
+        GICD_ISENABLER(reg) |= (1UL << bit);
+    }
 }
 
 void gic_disable_irq(int irq) {
     int reg = irq / 32;
     int bit = irq % 32;
 
-    GICD_ICENABLER(reg) = (1UL << bit);
+    if (irq < 32) {
+        GICR_ICENABLER0(0) |= (1UL << irq);
+    } else {
+        GICD_ICENABLER(reg) |= (1UL << bit);
+    }
 }
 
 bool gic_irq_pending(int irq) {
     int reg = irq / 32;
     int bit = irq % 32;
+    uint32_t pend = 0;
 
-    if (GICD_ISPENDR(reg)) {
-        return true;
+
+    if (irq < 32) {
+        pend = GICR_ICPENDR0(0) = (1UL << irq);
+    } else {
+        pend = GICD_ICPENDR(reg);
     }
 
+    if (pend & (1UL << bit)) {
+        return true;
+    }
+    
     return false;
 }
 
-void gic_set_irq_priority(int irq, uint8_t priority) {
-    int reg = irq / 32;
-    int shift = (irq % 4) * 8;
-    uint32_t val = 0;
+void gic_set_irq_priority(int irq, uint32_t priority) {
+    int reg = irq / 4;
+    int bit = (irq % 4) * 8;
+    uint32_t curr_priority = 0;
 
-    val = GICD_IPRIORITYR(reg);
 
-    val &= ~(0xFF << shift);
-    val |= (priority << shift);
+    if (priority > 0xFF) {
+        priority = 0xFF;
+    }
 
-    GICD_IPRIORITYR(reg) = val;
+    if (irq < 32) {
+        curr_priority = GICR_IPRIORITYR(0, reg);
+
+        curr_priority &= ~(0xFF << bit);
+        curr_priority |= (priority << bit);
+
+        GICR_IPRIORITYR(0, reg) = curr_priority;
+    } else {
+        curr_priority = GICD_IPRIORITYR(reg);
+
+        curr_priority &= ~(0xFF << bit);
+        curr_priority |= (priority << bit);
+
+        GICD_IPRIORITYR(reg) = curr_priority;
+    }
 }
 
-void gic_trigger_irq(int irq) {
-    uint32_t sgi_val = (0UL << 16UL) | (irq);
-    GICD_SGIR = sgi_val;
+void gic_set_irq_group(int irq, uint32_t group) {
+    int reg = irq / 32;
+    int bit = irq % 32;
+
+
+    if (irq < 32) {
+        if (group == 0) {
+            GICR_IGROUP0(0) &= ~(1UL << bit);
+        } else {
+            GICR_IGROUP0(0) |= (1UL << bit);
+        }
+    } else {
+        if (group == 0) {
+            // Secure group
+            GICD_IGROUPR(reg) &= ~(1UL << bit);
+        } else {
+            // Non-secure group
+            GICD_IGROUPR(reg) |= (1UL << bit);
+        }
+    }
 }
--- kernel/include/devices/gicv3.h
+++ kernel/include/devices/gicv3.h
@@ -16,11 +16,12 @@
 
 #define ICC_PMR_ALL     0xFF
 
+#define ICC_IGRPEN0_ENABLE  (1UL << 1UL)
+#define ICC_IGRPEN1_ENABLE  (1UL << 1UL)
+
 #define ICC_SRE_ENABLE      (1UL << 0UL)
 #define ICC_SRE_DFB_ENABLE  (1UL << 1UL)
 #define ICC_SRE_DIB_ENABLE  (1UL << 2UL)
-
-#define ICC_IGRPEN1_ENABLE  (1UL << 0UL)
 
 #define ICC_CTLR_CBPR           (1UL << 0UL)
 #define ICC_CTLR_EOI_MODE       (1UL << 1UL)
@@ -55,8 +56,8 @@
 #define GICC_CTLR_IRQ_BYPASS_GRP1   (1UL << 6UL)
 #define GICC_CTLR_EIO_MODE_NS       (1UL << 9UL)
 
-#define GICR_WAKER_CPU_SLEEP        (1UL << 0UL)
-#define GICR_WAKER_CHILDREN_SLEEP   (1UL << 1UL)
+#define GICR_WAKER_CPU_SLEEP        (1UL << 1UL)
+#define GICR_WAKER_CHILDREN_SLEEP   (1UL << 2UL)
 
 #define GICR_CTLR_ENABLE_LPI        (1UL << 0UL)
 #define GICR_CTLR_CES               (1UL << 1UL)
@@ -95,49 +96,35 @@
 #define GICD_SPENDSGIR(x)   (*(volatile uint32_t *)(GIC_DIST_BASE + 0x0F20 + (x) * 4))      // SGI set-pending register
 #define GICD_INMIR(x)       (*(volatile uint32_t *)(GIC_DIST_BASE + 0x0F80 + (x) * 4))      // Non-maskable interrupts
 
-#ifdef GIC_v2
-#define GICC_CTLR           (*(volatile uint32_t *)(GIC_CPU_BASE + 0x0000))                 // CPU interface control register
-#define GICC_PMR            (*(volatile uint32_t *)(GIC_CPU_BASE + 0x0004))                 // Interrupt priority mask
-#define GICC_BPR            (*(volatile uint32_t *)(GIC_CPU_BASE + 0x0008))                 // Binary point register
-#define GICC_IAR            (*(volatile uint32_t *)(GIC_CPU_BASE + 0x000C))                 // Interrupt ACK register
-#define GICC_EOIR           (*(volatile uint32_t *)(GIC_CPU_BASE + 0x0010))                 // End of interrupt register
-#define GICC_RPR            (*(volatile uint32_t *)(GIC_CPU_BASE + 0x0014))                 // Running priority register
-#define GICC_HPPIR          (*(volatile uint32_t *)(GIC_CPU_BASE + 0x0018))                 // Highest priority interrupt register
-#define GICC_STATUSR        (*(volatile uint32_t *)(GIC_CPU_BASE + 0x002C))                 // Error reporting status register
-#define GICC_APR(x)         (*(volatile uint32_t *)(GIC_CPU_BASE + 0x00D0 + (x) * 4))       // Active priorities register
-#define GICC_NSAPR(x)       (*(volatile uint32_t *)(GIC_CPU_BASE + 0x00E0 + (x) * 4))       // Non-secure active priorities register
-#define GICC_IIDR           (*(volatile uint32_t *)(GIC_CPU_BASE + 0x00FC))                 // CPU interface identification register
-#define GICC_DIR            (*(volatile uint32_t *)(GIC_CPU_BASE + 0x1000))                 // Deactivate interrupt register
-#endif
-
-#define GICR_CTLR           (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0000))              // Redistributor control register
-#define GICR_IIDR           (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0004))              // Implementor idenification register
-#define GICR_TYPR           (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0008))              // Redistributor register type
-#define GICR_STATUSR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0010))              // Error reporting register
-#define GICR_WAKER          (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0014))              // Redistributor wake register
-#define GICR_MPAMIDR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0018))              // Report maximum PARTID and PMG register
-#define GICR_PARTIDR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x001C))              // Set PARTID and PMG register
-#define GICR_SETLPIR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0040))              // Set LPI pending register
-#define GICR_CLRLIPR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0048))              // Clear LPI pending register
-#define GICR_PROPBASER      (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0070))              // Redistributor properties base address register
-#define GICR_PENDBASER      (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0078))              // Redistributor LPI pending table base address register
-#define GICR_INVLPIR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x00A0))              // Redistributor invaliate LPI register
-#define GICR_INVALLR        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x00B0))              // Redistributor invalidate all register
-#define GICR_SYNCR          (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x00C0))              // Redistributor synchronize register
-#define GICR_IGROUP0        (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0080))              // Interrupt group register 0
-#define GICR_ISENABLER0     (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0100))              // Interrupt set-enable register 0
-#define GICR_ICENABLER0     (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0180))              // Interrupt clear-enable register 0
-#define GICR_ISPENDR0       (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0200))              // Interrupt set-pend register 0
-#define GICR_ICPENDR0       (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0280))              // Interrupt clear-pend register 0
-#define GICR_ISACTIVER0     (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0300))              // Interrupt set-active register 0
-#define GICR_ICACTICER0     (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0380))              // Interrupt clear-active register 0
-#define GICR_IPRIORITYR(x)  (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0384 + (x) * 4))    // Interrupt priority register
-#define GICR_ICFGR0         (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0C00))              // SGI configuration register
-#define GICR_ICFGR1         (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0C04))              // PPI configuration register
-#define GICR_IGRPMODR       (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0D00))              // Interrupt group modifier register 0
-#define GICR_NSACR          (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0E00))              // Non-secure access control register
-#define GICR_NIMIR0         (*(volatile uint32_t *)(GIC_REDIST_BASE + 0x0F80))              // Non-maskable interrupt register for PPI's and SGI's
-
+#define GICR_RD_BASE(cpu)  (GIC_REDIST_BASE + (cpu) * 0x20000)
+#define GICR_SGI_BASE(cpu) (GICR_RD_BASE(cpu) + 0x10000)
+#define GICR_CTLR(cpu)       (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0000))
+#define GICR_IIDR(cpu)       (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0004))
+#define GICR_TYPER(cpu)      (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0008))
+#define GICR_STATUSR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0010))
+#define GICR_WAKER(cpu)      (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0014))
+#define GICR_MPAMIDR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0018))
+#define GICR_PARTIDR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x001C))
+#define GICR_SETLPIR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0040))
+#define GICR_CLRLIPR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0048))
+#define GICR_PROPBASER(cpu)  (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0070))
+#define GICR_PENDBASER(cpu)  (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x0078))
+#define GICR_INVLPIR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x00A0))
+#define GICR_INVALLR(cpu)    (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x00B0))
+#define GICR_SYNCR(cpu)      (*(volatile uint32_t *)(GICR_RD_BASE(cpu) + 0x00C0))
+#define GICR_IGROUP0(cpu)    (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0080))
+#define GICR_ISENABLER0(cpu) (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0100))
+#define GICR_ICENABLER0(cpu) (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0180))
+#define GICR_ISPENDR0(cpu)   (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0200))
+#define GICR_ICPENDR0(cpu)   (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0280))
+#define GICR_ISACTIVER0(cpu) (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0300))
+#define GICR_ICACTICER0(cpu) (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0380))
+#define GICR_IPRIORITYR(cpu, x) (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0384 + (x) * 4))
+#define GICR_ICFGR0(cpu)     (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0C00))
+#define GICR_ICFGR1(cpu)     (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0C04))
+#define GICR_IGRPMODR(cpu)   (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0D00))
+#define GICR_NSACR(cpu)      (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0E00))
+#define GICR_NIMIR0(cpu)     (*(volatile uint32_t *)(GICR_SGI_BASE(cpu) + 0x0F80))
 
 
 
@@ -151,9 +138,11 @@
 
 void gic_init(void);
 void gic_enable_irq(int irq);
-void gic_disable_irqI(int irq);
-void gic_set_irq_priority(int irq, uint8_t priority);
-void gic_trigger_irq(int irq);
+void gic_disable_irq(int irq);
+void gic_set_irq_priority(int irq, uint32_t priority);
 bool gic_irq_pending(int irq);
+int gic_ack_irq(void);
+void gic_clear_irq(int irq);
+void gic_set_irq_group(int irq, uint32_t group);
 
 #endif
