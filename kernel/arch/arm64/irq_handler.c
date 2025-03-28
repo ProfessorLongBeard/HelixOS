fdb7b2e100ca8253d1b8a1ae500f1dfc848752b3
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-28 03:13:57 -0700

    IRQ: Ensure checking of higher priority IRQ's or fallback to lower priority IRQ's

--- kernel/arch/arm64/irq_handler.c
+++ kernel/arch/arm64/irq_handler.c
@@ -14,18 +14,46 @@ static void (*irq_handler_table[MAX_IRQ_ID])(void) = {0};
 void irq_register(int irq, void (*irq_handler)(void)) {
     assert(irq <= MAX_IRQ_ID);
 
+    printf("Registering IRQ handler %d\n", irq);
     irq_handler_table[irq] = irq_handler;
 }
 
 void irq_handler(void) {
-    int irq = __icc_iar1_read();
-    assert(irq <= MAX_IRQ_ID);
+    // Check for higher priority IRQ's
+    int irq_id = gic_ack_hppi_irq();
 
-    printf("IRQ: Got IRQ ID: %d! Exectuting IRQ vector\n", irq);
+    if (irq_id == -1) {
+        // Fallback to IAR if no higher priorty IRQ was found
+        irq_id = gic_ack_irq();
 
-    if (irq_handler_table[irq]) {
-        irq_handler_table[irq]();
+        if (irq_id == -1) {
+            // If no lower priority IRQ is found then bail out
+            printf("No valid IRQ ID's found!\n");
+            return;
+        }
+
+        if (irq_handler_table[irq_id] == NULL) {
+            // Can't handle IRQ's with no registered IRQ handler
+            printf("IRQ: %d unregistered!\n");
+            return;
+        }
+
+        // Mask interupts and call IRQ handler
+        __daif_set();
+        irq_handler_table[irq_id]();
+
     } else {
-        printf("IRQ: %d not registered!\n", irq);
+        if (irq_handler_table[irq_id] == NULL) {
+            // Can't handle IRQ's with no registered IRQ handler
+            printf("IRQ: %d unregistered!\n");
+            return;
+        }
+
+        // Mask interrupts and call IRQ handler
+        __daif_set();
+        irq_handler_table[irq_id]();
     }
+
+    // Unmask IRQ's and return
+    __daif_clr();
 }
