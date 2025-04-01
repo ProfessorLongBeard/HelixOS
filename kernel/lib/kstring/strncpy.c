057cccdbe21f7cb23ab50a08fee8d71338999dce
ProfessorLongBeard <austingarrick752@gmail.com>
2025-03-31 20:56:56 -0700

    strncpy: Misc

--- kernel/lib/kstring/strncpy.c
+++ kernel/lib/kstring/strncpy.c
@@ -8,13 +8,17 @@
 
 
 char *strncpy(char *dst, const char *src, size_t length) {
+    size_t i = 0;
     char *d = (char *)dst;
     char *s = (char *)src;
 
 
-
-    for (size_t i = 0; i < length; i++) {
+    for (i = 0; i < length && s[i] != '\0'; i++) {
         d[i] = s[i];
+    }
+
+    for (; i < length; i++) {
+        d[i] = '\0';
     }
 
     return dst;
