90e258e727ff0fc3f1baebe03bfbae052c6874f7
ProfessorLongBeard <austingarrick752@gmail.com>
2025-05-02 23:22:09 -0700

    TIMER: Ensure safe atomic operations for concurrent access

--- kernel/devices/timer.c
+++ kernel/devices/timer.c
@@ -4,10 +4,14 @@
 #include <arch.h>
 #include <devices/gicv3.h>
 #include <devices/timer.h>
+#include <spinlock.h>
 
 
 
 
+
+
+static spinlock_t s;
 static volatile uint64_t timer_freq_hz = 0;
 static volatile uint64_t saved_ticks = 0;
 static volatile uint64_t sys_ticks = 0;
@@ -20,6 +24,8 @@ static volatile uint64_t sys_ticks = 0;
 void timer_init(void) {
     timer_disable();
     timer_freq_hz = timer_get_freq();
+
+    spinlock_init(&s);
 
     printf("Timer frequency: %lluHz\n", timer_freq_hz);
 
@@ -35,15 +41,25 @@ void timer_set(uint64_t ms) {
     uint64_t deadline = curr_time + ticks;
     saved_ticks = ticks;
 
+
+
+    spinlock_acquire(&s);
+
+    // Disable timer
+    timer_disable();
+
     // Set timer
     __cntv_cval_write(deadline);
 
     // Enable timer
     timer_enable();
+
+    spinlock_release(&s);
 }
 
 void timer_disable(void) {
     uint64_t cntv = __cntv_ctl_read();
+
     cntv &= ~(CNTV_CTL_ENABLE);
     cntv |= CNTV_CTL_IMASK;
 
@@ -79,21 +95,34 @@ uint64_t timer_get_sys_ticks(void) {
 }
 
 void timer_reset_sys_ticks(void) {
+    spinlock_acquire(&s);
+
+    // Reset sys_tick count
     sys_ticks = 0;
+
+    spinlock_release(&s);
 }
 
 void timer_sleep(uint64_t delay) {
     uint64_t start = timer_get_sys_ticks();
 
+
+    spinlock_acquire(&s);
+
     while((timer_get_sys_ticks() - start) < delay) {
-        wfi();
+        __asm__ volatile("wfi\n\t");
     }
+
+    spinlock_release(&s);
 }
 
 void timer_irq_handler(void) {
     uint64_t curr_time = timer_virt_get_time();
     uint64_t deadline = curr_time + saved_ticks;
     
+
+    spinlock_acquire(&s);
+
     timer_disable();
 
     sys_ticks++;
@@ -101,4 +130,6 @@ void timer_irq_handler(void) {
     __cntv_cval_write(deadline);
 
     timer_enable();
+
+    spinlock_release(&s);
 }
