9628c32687c01f71bc3ddb7711482ffd6c4573aa
ProfessorLongBeard <austingarrick752@gmail.com>
2025-05-25 15:17:17 -0700

    Ensure spinlocks use memory barriers

--- kernel/locking/spinlock.c
+++ kernel/locking/spinlock.c
@@ -19,10 +19,14 @@ void spinlock_acquire(spinlock_t *s) {
     while(atomic_flag_test_and_set(&s->lock)) {
         __asm__ volatile("wfi\n\t");
     }
+
+    __asm__ volatile("dmb ish\n\t" ::: "memory");
 }
 
 void spinlock_release(spinlock_t *s) {
     assert(s != NULL);
+
+    __asm__ volatile("dmb ish\n\t" ::: "memory");
 
     atomic_flag_clear(&s->lock);
 }
